059f173bfe904c6761416785f0e93b4d0a3a9538
Sam Streed <embersteelstreus@gmail.com>
2019-01-10 16:42:17 -0600

    Firguring out Connections.

--- Assets/Labrys/Editor/FeatureEditor/Connection.cs
+++ Assets/Labrys/Editor/FeatureEditor/Connection.cs
@@ -5,24 +5,65 @@ namespace Labrys.Editor.FeatureEditor
 {
 	public class Connection
 	{
-		private Tile[] members;
+		public Vector2Int Position { get; private set; }
+		public Vector2Int Direction { get; private set; }
 
-		public Connection(Tile self, Tile other, LinkDirection direction)
+		public static Vector2Int LinkDirectionToVector2Int(LinkDirection dir)
 		{
-			members = new Tile[2];
-			members[0] = self;
-			members[1] = other;
+			switch (dir)
+			{
+			case LinkDirection.Right:
+				return Vector2Int.right;
+			case LinkDirection.Up:
+				return Vector2Int.up;
+			case LinkDirection.Left:
+				return Vector2Int.left;
+			case LinkDirection.Down:
+				return Vector2Int.down;
+			default:
+				return Vector2Int.zero;
+			}
+		}
 
+		public static bool operator ==(Connection left, Connection right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(Connection left, Connection right)
+		{
+			return !left.Equals(right);
+		}
+
+		public Connection(Vector2Int postion, Vector2Int direction)
+		{
+			Position = postion;
+			Direction = direction;
 		}
 
 		public void Draw()
 		{
 
+			GUI.Box(new Rect(Position, new Vector2(1f, 1f)), "");
+			Handles.BeginGUI();
+			Handles.DrawWireDisc(new Vector3(Position.x, Position.y), Vector3.forward, 1f);
+			Handles.EndGUI();
 		}
 
 		public bool HandleEvent(Event e)
 		{
 			return false;
+		}
+
+		public override bool Equals(object obj)
+		{
+			Connection other = (Connection)obj;
+			return Position == other.Position && Direction == other.Direction;
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
 		}
 	}
 }
