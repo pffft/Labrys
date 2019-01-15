573e5e13be52f908382c20a73678380ff10625bc
Sam Streed <embersteelstreus@gmail.com>
2019-01-15 15:02:08 -0600

    Still working on how the hell connections should work...

--- Assets/Labrys/Editor/FeatureEditor/LinkDirection.cs
+++ Assets/Labrys/Editor/FeatureEditor/LinkDirection.cs
@@ -3,8 +3,12 @@
 	public enum LinkDirection
 	{
 		Right = 0,
-		Up = 1,
-		Left = 2,
-		Down = 3
+		UpRight = 1,
+		Up = 2,
+		UpLeft = 3,
+		Left = 4,
+		DownLeft = 5,
+		Down = 6,
+		DownRight = 7
 	}
 }
