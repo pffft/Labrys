08461712d53d8827108c5c929e00c27395cfa132
Sam Streed <embersteelstreus@gmail.com>
2019-01-28 15:56:04 -0600

    Added menubar. Currently only stubs for all functionality.

--- Assets/Labrys/Editor/FeatureEditor/MainWindow.cs
+++ Assets/Labrys/Editor/FeatureEditor/MainWindow.cs
@@ -12,8 +12,6 @@ namespace Labrys.Editor.FeatureEditor
 			window.titleContent = new GUIContent ("Labrys Feature Editor");
 		}
 
-		private Vector2 drag;
-
 		private void OnEnable()
 		{
 			EditorGrid.GetInstance().viewport = position;
@@ -26,6 +24,34 @@ namespace Labrys.Editor.FeatureEditor
 			HandleEvent (Event.current);
 
 			EditorGrid.GetInstance().Draw ();
+
+			EditorGUI.DrawRect(new Rect(0f, 0f, position.width, 22f), GUI.color);
+			if(GUI.Button(new Rect(0f, 0f, 64f, 20f), "File"))
+			{
+				GenericMenu fileMenu = new GenericMenu();
+				fileMenu.AddItem(new GUIContent("New Feature..."), false, CreateNewFeature);
+				fileMenu.AddItem(new GUIContent("Open Feature..."), false, OpenFeature);
+				fileMenu.AddItem(new GUIContent("Save"), false, SaveFeature);
+				fileMenu.AddItem(new GUIContent("Save As..."), false, SaveAsFeature);
+
+				fileMenu.ShowAsContext();
+			}
+
+			if (GUI.Button(new Rect(70f, 0f, 64f, 20f), "Edit"))
+			{
+				GenericMenu editMenu = new GenericMenu();
+				editMenu.AddDisabledItem(new GUIContent("TODO"));
+
+				editMenu.ShowAsContext();
+			}
+
+			if(GUI.Button(new Rect(140f, 0f, 64f, 20f), "View"))
+			{
+				GenericMenu viewMenu = new GenericMenu();
+				viewMenu.AddDisabledItem(new GUIContent("TODO"));
+
+				viewMenu.ShowAsContext();
+			}
 
 			if (GUI.changed)
 				Repaint ();
@@ -60,6 +86,26 @@ namespace Labrys.Editor.FeatureEditor
 			default:
 				return;
 			}
+		}
+
+		private void CreateNewFeature()
+		{
+			Debug.Log("TODO");
+		}
+
+		private void OpenFeature()
+		{
+			Debug.Log("TODO");
+		}
+
+		private void SaveFeature()
+		{
+			Debug.Log("TODO");
+		}
+
+		private void SaveAsFeature()
+		{
+			Debug.Log("TODO");
 		}
 
 		private void HandleContextMenu(Vector2 mousePos)
