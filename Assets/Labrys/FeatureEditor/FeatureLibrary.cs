48975b88d9de0639c5fe8063928f398b6bb55447
Sam Streed <embersteelstreus@gmail.com>
2019-09-11 16:45:34 -0500

    Working prototype. Generated names aren't working properly.

--- Assets/Labrys/FeatureEditor/FeatureLibrary.cs
+++ Assets/Labrys/FeatureEditor/FeatureLibrary.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -9,24 +8,18 @@ using UnityEngine;
 
 namespace Labrys.FeatureEditor
 {
-    public class FeatureLibrary : ScriptableObject, IEnumerable<FeatureAsset>
+    public class FeatureLibrary : ScriptableObject, IEnumerable<FeatureAsset>, ISerializationCallbackReceiver
     {
+        [field: SerializeField]
+        //[field: HideInInspector]
+        public string TargetDirectory { get; private set; }
+
         private Dictionary<string, ushort> nameToFAID;
         private FeatureAsset[] features;
 
         public int Count => features.Length;
 
-        public FeatureAsset this[ushort faid]
-        {
-            get
-            {
-                if (faid >= 0 && faid < features.Length)
-                {
-                    return features[faid];
-                }
-                return null;
-            }
-        }
+        public FeatureAsset this[ushort faid] => Contains(faid) ? features[faid] : null;
 
         public FeatureAsset this[string name]
         {
@@ -38,41 +31,64 @@ namespace Labrys.FeatureEditor
         [MenuItem("Assets/Create/Labrys/Feature Library")]
         private static void FromDirectory()
         {
-            string loadPath = $"Assets/{EditorUtility.OpenFolderPanel("Select target folder", "Assets", "").Replace(Application.dataPath, "")}";
+            string loadPath = $"Assets/{EditorUtility.OpenFolderPanel("Select target Feature folder", "Assets", "").Replace(Application.dataPath, "")}";
             string savePath = $"{AssetDatabase.GetAssetPath(Selection.activeInstanceID)}/NewFeatureLibrary.asset";
 
-            Debug.Log($"Load path: {loadPath}");
-            Debug.Log($"Save path: {savePath}");
-            string[] assets = AssetDatabase.FindAssets("t:FeatureAsset", new string[] { loadPath });
+            string[] assetGUIDs = AssetDatabase.FindAssets($"t:{nameof(FeatureAsset)}", new string[] { loadPath });
 
             FeatureLibrary library = CreateInstance<FeatureLibrary>();
-            foreach (string asset in assets)
+            library.TargetDirectory = loadPath;
+            library.features = new FeatureAsset[assetGUIDs.Length];
+            foreach (string guid in assetGUIDs)
             {
-                library.Add(asset, AssetDatabase.LoadAssetAtPath<FeatureAsset>(asset));
+                string path = AssetDatabase.GUIDToAssetPath(guid);
+                string name = path.Replace(library.TargetDirectory, "").Replace(".asset", "");
+                library.Add(name, AssetDatabase.LoadAssetAtPath<FeatureAsset>(path));
             }
 
             ProjectWindowUtil.CreateAsset(library, savePath);
         }
 #endif
 
-        private FeatureLibrary(int capacity)
+        private FeatureLibrary()
         {
             nameToFAID = new Dictionary<string, ushort>();
-            features = new FeatureAsset[capacity];
+            features = new FeatureAsset[0];
         }
+
+#if UNITY_EDITOR
+        public void Refresh()
+        {
+            string[] assetGUIDs = AssetDatabase.FindAssets($"t:{nameof(FeatureAsset)}", new string[] { TargetDirectory });
+            nameToFAID.Clear();
+            features = new FeatureAsset[assetGUIDs.Length];
+
+            foreach (string guid in assetGUIDs)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guid);
+                Add(path.Replace(TargetDirectory, "").Replace(".asset", ""), AssetDatabase.LoadAssetAtPath<FeatureAsset>(path));
+            }
+        }
+#endif
 
         private bool Add(string name, FeatureAsset feature)
         {
             if (name == null || feature == null)
                 return false;
 
-            nameToFAID.Add(name, (ushort)nameToFAID.Count);
             features[nameToFAID.Count] = feature;
+            nameToFAID.Add(name, (ushort)nameToFAID.Count);
 
             return true;
         }
 
+        public FeatureAsset GetFeature(string name) => this[name];
+
+        public FeatureAsset GetFeature(ushort faid) => this[faid];
+
         public bool Contains(string name) => nameToFAID.ContainsKey(name);
+
+        public bool Contains(ushort faid) => 0 <= faid && faid < features.Length;
 
         public ushort GetId(string name)
         {
@@ -92,5 +108,45 @@ namespace Labrys.FeatureEditor
         {
             return ((IEnumerable<FeatureAsset>)features).GetEnumerator();
         }
+
+#region SERIALIZATION
+        [SerializeField]
+        private List<string> serializedNames;
+        [SerializeField]
+        private List<FeatureAsset> serializedFeatures;
+
+        public void OnBeforeSerialize()
+        {
+            if (nameToFAID == null || features == null)
+                return;
+
+            serializedNames = new List<string>();
+            serializedFeatures = new List<FeatureAsset>();
+            foreach(KeyValuePair<string, ushort> pair in nameToFAID)
+            {
+                serializedNames.Add(pair.Key);
+                serializedFeatures.Add(features[pair.Value]);
+            }
+            nameToFAID = null;
+            features = null;
+        }
+
+        public void OnAfterDeserialize()
+        {
+            if (serializedNames == null 
+                || serializedFeatures == null 
+                || serializedNames.Count != serializedFeatures.Count)
+                return;
+
+            nameToFAID = new Dictionary<string, ushort>();
+            features = new FeatureAsset[serializedFeatures.Count];
+            for (int i = 0; i < serializedNames.Count; i++)
+            {
+                Add(serializedNames[i], serializedFeatures[i]);
+            }
+            serializedNames = null;
+            serializedFeatures = null;
+        }
+#endregion
     }
 }
